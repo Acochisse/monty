477088d113712ef81bf251b87fb631880ac6cb33
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-17 12:11:50 -0500

    adding mod

--- math.c
+++ math.c
@@ -123,3 +123,34 @@ void _div(stack_t **stack, unsigned int line_number)
 	free(current);
 
 }
+
+/**
+ * _mod - modulo top two elements and replaces them with the sum
+ * @stack: pointer to stack
+ * @line_number: line number
+ *
+ * Return: none excecpt -1 on fail
+ */
+
+void _mod(stack_t **stack, unsigned int line_number)
+{
+
+	int i, j, ret;
+	stack_t *current = *stack;
+
+	if (len(stack) < 2)
+	{
+		printf("L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	i = current->n;
+	j = current->next->n;
+
+	ret = (j % i);
+
+	current->next->n = ret;
+	*stack = current->next;
+	free(current);
+
+}
